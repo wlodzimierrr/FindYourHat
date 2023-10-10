195ec365746ef097230caba0677874c84a7e9efe
wlodzimierrr <wlodzimierrr@github.com>
2023-10-10 14:46:15 +0100

    move function added

--- starting/main.js
+++ starting/main.js
@@ -1,8 +1,14 @@
-const prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
 
 class Field {
     constructor(height, width, holePercentage) {
-        this.field = this.generateField(height, width, holePercentage);
+        this.field = field;
         this.playerPosition = { x: 0, y: 0 };
     }
 
@@ -14,7 +20,7 @@ class Field {
     }
 
     // generate field, two dimenisonal array with random holes and hidden hat
-    generateField(height, width, holePercentage) {
+    static generateField(height, width, holePercentage) {
         const field = Array.from({ length: height }, () =>
             Array.from({ length: width }, () => {
                 const isHole = Math.random() < holePercentage / 100;
@@ -24,9 +30,29 @@ class Field {
         const hatX = Math.floor(Math.random() * width);
         const hatY = Math.floor(Math.random() * height);
         field[hatY][hatX] = '^';
-        return field;
+        return new Field(field);
     }
 
+    move(direction) {
+        const { x, y } = this.playerPosition;
+        switch (direction.toLowerCase()) {
+            case 'w':
+                this.playerPosition = { x: x, y: y - 1 };
+                break;
+            case 's':
+                this.playerPosition = { x: x, y: y + 1 };
+                break;
+            case 'a':
+                this.playerPosition = { x: x - 1, y: y };
+                break;
+            case 'd':
+                this.playerPosition = { x: x + 1, y: y };
+                break;
+            default:
+                break;
+        }
+    }
+    
 
 
 }
