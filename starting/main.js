d31dc6a041d37dd2061d732a6c63e6c287f4b395
wlodzimierrr <wlodzimierrr@github.com>
2023-10-10 14:23:09 +0100

    generateField updated

--- starting/main.js
+++ starting/main.js
@@ -13,9 +13,22 @@ class Field {
         return this.field.map(row => row.join('')).join('\n');
     }
 
-    // generate field, two dimenisonal array 
+    // generate field, two dimenisonal array with random holes and hidden hat
     generateField(height, width, holePercentage) {
-        
+        const field = Array.from({ length: height }, () =>
+            Array.from({ length: width }, () => {
+                const isHole = Math.random() < holePercentage / 100;
+                return isHole ? 'O' : '░';
+            })
+        );
+        const hatX = Math.floor(Math.random() * width);
+        const hatY = Math.floor(Math.random() * height);
+        field[hatY][hatX] = '^';
+        return field;
     }
+
+
+
+}
 
 
